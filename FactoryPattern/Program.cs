850caff25e338e4f7f8ceea392634ef999eec6d0
jose valdez <josezepedavaldez30@gmail.com>
2023-02-22 20:51:02 -0800

    initial commit

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int wheelCount;
+            bool input = false;
+            do
+            {
+                Console.WriteLine("enter the amount of tires for the vehicle you want to create");
+
+                input = int.TryParse(Console.ReadLine(), out wheelCount);
+            } while (!input);
+            var vehicle = Vehicle_Factory.GetVehicle(wheelCount);
+            vehicle.Drive();
+            Console.WriteLine("your on your way");
         }
     }
 }
---
+++ FactoryPattern/Vehicle Factory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class Vehicle_Factory
+    {
+        public static IVehicle GetVehicle(int wheeCount)
+        {
+            switch (wheeCount)
+            { 
+                case 2:
+                    return new Motorcycle();
+                    case 3:
+                    return new ThreeWheeler();
+                case 4:
+                    return new Car();
+                default:
+                    return new Car();
+        }
+      }
+    }
+}
